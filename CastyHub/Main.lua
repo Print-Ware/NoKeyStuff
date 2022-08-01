66ffa27ac928c3a94e4bf58eb6f722919ef75762
Print-Ware <110134262+Print-Ware@users.noreply.github.com>
2022-08-01 03:23:17 -0700

    Create Main.lua

---
+++ CastyHub/Main.lua
@@ -0,0 +1,28 @@
+local placeid = game.PlaceId
+
+
+
+if placeid == 286090429 then 
+-- Arsenal
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Print-Ware/NoKeyStuff/main/CastyHub/Arsenal.txt"))()
+
+
+
+end
+
+if placeid == 606849621 then
+    --jailbreak
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Print-Ware/NoKeyStuff/main/CastyHub/JailBreak.txt"))()
+
+
+
+
+end
+
+if placeid == 142823291 then
+ -- mm2
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Print-Ware/NoKeyStuff/main/CastyHub/MM2.txt"))()
+
+
+
+end
